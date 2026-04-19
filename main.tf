41602d6bd3826b17d2fb2552510d0b6fe61b4775
Abdul Basheer <73757834+Abdulbasheermyself@users.noreply.github.com>
2026-04-19 21:52:25 +0530

    Update source URL for aws-ec2 module

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "aws-ec2" {
-  source  = "test_aws_org_98/aws-ec2/aws"
+  source  = "app.terraform.io/test_aws_org_98/aws-ec2/aws"
   version = "1.1.0"
 
   ami           = "ami-04680790a315cd58d"
