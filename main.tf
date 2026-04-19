f09f04f1f2d30c8875ddef7cf41f361c9eec4471
Abdul Basheer <73757834+Abdulbasheermyself@users.noreply.github.com>
2026-04-19 21:46:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,8 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
+module "aws-ec2" {
+  source  = "test_aws_org_98/aws-ec2/aws"
+  version = "1.1.0"
 
-provider "aws" {
-  region = "ap-south-1"
+  ami           = "ami-04680790a315cd58d"
+  instance_type = "t2.micro"
+  name          = "demo-instance"
 }
